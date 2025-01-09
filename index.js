70f5d10f22bd54b03ba091ed930a9b2ed1fe3524
wposso <willintonpiedrahita016@gmail.com>
2025-01-09 12:08:04 -0500

    Feature cloud/localhost

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mssql = require('mssql');
 const cors = require('cors');
@@ -10,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const dbConfig = {
+const dbConfigLocal = {
     user: 'sa',
     password: 'masterkey',
     server: 'ADMINSYSTEM',
@@ -18,19 +17,35 @@ const dbConfig = {
     options: {
         encrypt: false,
         trustServerCertificate: true,
-    }
+    },
 };
+
+const dbConfigAzure = {
+    user: 'dobleu',
+    password: 'Medellin2025.',
+    server: 'cloudmovile.database.windows.net',
+    database: 'Node js',
+    options: {
+        encrypt: true,
+        trustServerCertificate: false,
+    },
+};
+
+const isCloud = process.env.USE_CLOUD_DB === 'true';
+const dbConfig = isCloud ? dbConfigAzure : dbConfigLocal;
 
 let dbPool;
 
-mssql.connect(dbConfig)
-    .then(pool => {
-        dbPool = pool;
-        console.log('Conexión a la base de datos establecida');
-    })
-    .catch(err => {
-        console.error('Error al conectar a la base de datos', err);
-    });
+async function connectToDatabase() {
+    try {
+        dbPool = await mssql.connect(dbConfig);
+        console.log(`Conexión a la base de datos ${isCloud ? 'en la nube (Azure)' : 'local'} establecida`);
+    } catch (err) {
+        console.error('Error al conectar a la base de datos:', err);
+    }
+}
+
+connectToDatabase();
 
 app.use((req, res, next) => {
     if (!dbPool) {
