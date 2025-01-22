2f1c3e55cfe7ef1c09c2cd9d0a66bec2a83c34c3
wposso <willintonpiedrahita016@gmail.com>
2025-01-22 11:03:40 -0500

    Localhost (infratech) v.1.8

--- routes/login.js
+++ routes/login.js
@@ -14,30 +14,31 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
-    const { email, password, firstname, lastname, phone, profession } = req.body;
+// router.post('/', async (req, res) => {
+//     const { email, password, firstname, lastname, phone, profession } = req.body;
 
-    if (!email || !password || !firstname || !lastname || !phone || !profession) {
-        return res.status(400).send('Todos los datos son requeridos');
-    }
+//     if (!email || !password || !firstname || !lastname || !phone || !profession) {
+//         return res.status(400).send('Todos los datos son requeridos');
+//     }
 
-    try {
-        const pool = req.db;
-        const result = await pool.request()
-            .input('Email', mssql.VarChar, email)
-            .input('Password', mssql.VarChar, password)
-            .input('FirstName', mssql.VarChar, firstname)
-            .input('LastName', mssql.VarChar, lastname)
-            .input('Phone', mssql.VarChar, phone)
-            .input('Profession', mssql.VarChar, profession)
-            .execute('SP_CREARUSUARIO');
+//     try {
+//         const pool = req.db;
+//         const result = await pool.request()
+//             .input('Email', mssql.VarChar, email)
+//             .input('Password', mssql.VarChar, password)
+//             .input('FirstName', mssql.VarChar, firstname)
+//             .input('LastName', mssql.VarChar, lastname)
+//             .input('Phone', mssql.VarChar, phone)
+//             .input('Profession', mssql.VarChar, profession)
+//             .execute('SP_CREARUSUARIO');
 
-        res.json({ message: 'Usuario creado exitosamente' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error al crear el usuario');
-    }
-});
+//         console.log('Usuario creado:', result);
+//         res.json({ message: 'Usuario creado exitosamente' });
+//     } catch (err) {
+//         console.error('Error al ejecutar SP_CREARUSUARIO:', err.message, err.stack);
+//         res.status(500).send('Error al crear el usuario');
+//     }
+// });
 
 
 module.exports = router;
