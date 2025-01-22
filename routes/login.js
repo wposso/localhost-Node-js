a8ca27ea14894b1254d74df720900a76b9377dc3
wposso <willintonpiedrahita016@gmail.com>
2025-01-22 10:52:56 -0500

    Localhost (infratech) v.1.7

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ const mssql = require('mssql');
 router.get('/', async (req, res) => {
     try {
         const pool = req.db;
-        const result = await pool.request().query('SELECT * FROM Login');
+        const result = await pool.request().query('SELECT * FROM dbo.Login');
         res.json(result.recordset);
     } catch (err) {
         console.error(err);
@@ -15,10 +15,10 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password, firstname, lastname, phone, profession } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).send('Email y contraseña son requeridos');
+    if (!email || !password || !firstname || !lastname || !phone || !profession) {
+        return res.status(400).send('Todos los datos son requeridos');
     }
 
     try {
@@ -26,6 +26,10 @@ router.post('/', async (req, res) => {
         const result = await pool.request()
             .input('Email', mssql.VarChar, email)
             .input('Password', mssql.VarChar, password)
+            .input('FirstName', mssql.VarChar, firstname)
+            .input('LastName', mssql.VarChar, lastname)
+            .input('Phone', mssql.VarChar, phone)
+            .input('Profession', mssql.VarChar, profession)
             .execute('SP_CREARUSUARIO');
 
         res.json({ message: 'Usuario creado exitosamente' });
